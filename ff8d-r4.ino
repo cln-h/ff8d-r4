08489f247939e4ec9c611de18ae36ce7abc0c3bd
cln-h <cullen.hutchison@gmail.com>
2024-04-07 16:16:57 -0500

    Fix some wifi logic and print ip address

--- ff8d-r4.ino
+++ ff8d-r4.ino
@@ -95,6 +95,11 @@ long getWiFiSignalStrength() {
   return rssi;
 }
 
+void printIpAddress() {
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void attachServo(Servo &servo, int pin) {
   if (!servo.attached()) {
     servo.attach(pin);
@@ -146,41 +151,51 @@ void handleRoot() {
 void setup() {
   Serial.begin(9600);
 
+  delay(5000);
+
   while (!Serial) {
     ; // Wait for Serial port to connect
   }
 
-    if (WiFi.status() == WL_NO_MODULE) {
+  Serial.println("Serial started.");
+
+  if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     // don't continue
     // TODO: LED Indicator
     while (true);
-
-    String fv = WiFi.firmwareVersion();
-    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-      Serial.println("WARN: Firmware out of date");
-    }
-
-    while (status != WL_CONNECTED) {
-      Serial.print("Attempting to connect to WPA SSID: ");
-      Serial.println(ssid);
-
-      status = WiFi.begin(ssid, pass);
-
-      delay(10000);
-    }
-
-    Serial.println("Connected to WiFi");
-
-    long rssi = getWiFiSignalStrength();
-    Serial.print("Signal strength (RSSI): ");
-    Serial.println(rssi);
-
-    server.addRoute("/", handleRoot);
-    server.addRoute("/servo", handleServo);
-
-    server.begin();
   }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("WARN: Firmware out of date");
+  }
+
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+
+    status = WiFi.begin(ssid, pass);
+    
+    if (status == WL_CONNECT_FAILED) {
+      Serial.println("Failed to connect to WiFi... Retrying in 10 seconds.");
+    }
+
+    delay(10000);
+  }
+
+  Serial.println("Connected to WiFi");
+
+  long rssi = getWiFiSignalStrength();
+  Serial.print("Signal strength (RSSI): ");
+  Serial.println(rssi);
+  
+  printIpAddress();
+
+  server.addRoute("/", handleRoot);
+  server.addRoute("/servo", handleServo);
+
+  server.begin();
 }
 
 void loop() {
